f02612a9aa4a10c7fb6cb7b1ef32b7a1dfb63db8
Slava <slava@example.com>
2022-05-30 13:50:26 +0300

    commit

--- src/blocks/modules/burger/burger.js
+++ src/blocks/modules/burger/burger.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $('.header__navigation').addClass('open-menu');
     });
     $('.header__close-btn').click(function(){
-        $('.menu-burger__header').removeClass('open-menu');
+        $(".menu-burger__header").removeClass('open-menu');
         $('.header__navigation').removeClass('open-menu');
     });
 });
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,4 +1,5 @@
 import "%modules%/header/header";
-import "%modules%/footer/footer";
 import "%modules%/burger/burger";
 import "%modules%/slider/slider";
+import "%modules%/team-slider/team-slider";
+import "%modules%/publications/publications";
